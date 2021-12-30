b5ac6bcb92a78c84b28e314b079a63fe8d22bdfc
rychanya <rychanya@gmail.ru>
2021-12-30 10:14:25 +0300

    add qa answer test

--- tests/test_mongo_store.py
+++ tests/test_mongo_store.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 import pytest
 
-from qastorage.dto import QABaseDTO, QAGroupDTO, QATypeEnum
+from qastorage.dto import QAAnswerDTO, QABaseDTO, QAGroupDTO, QATypeEnum
 from qastorage.mongo_srore import MongoStore
 
 
@@ -15,6 +15,16 @@ def fake_base_dto():
 @pytest.fixture
 def fake_group_dto():
     return QAGroupDTO(all_answers=["1", "2", "3", "4"])
+
+
+@pytest.fixture
+def fake_answer_dto(fake_base_dto: QABaseDTO, fake_group_dto: QAGroupDTO):
+    return QAAnswerDTO(
+        base=fake_base_dto,
+        group=fake_group_dto,
+        answer=fake_group_dto.all_answers[1:2],
+        is_correct=True,
+    )
 
 
 def test_gorc_base(store: MongoStore, fake_base_dto: QABaseDTO):
@@ -46,3 +56,17 @@ def test_gorc_group(store: MongoStore, fake_group_dto: QAGroupDTO):
     id3 = store.get_or_create_group(new_dto, base_id)
     assert count_documents() == 1
     assert id3 == id
+
+
+def test_gorc_answer(store: MongoStore, fake_answer_dto: QAAnswerDTO):
+    def count_documents():
+        return store._answers_collection.count_documents({})
+
+    assert count_documents() == 0
+    qa1, is_new = store.get_or_create_qa(fake_answer_dto)
+    assert is_new is True
+    assert count_documents() == 1
+    qa2, is_new = store.get_or_create_qa(fake_answer_dto)
+    assert is_new is False
+    assert count_documents() == 1
+    qa1 == qa2

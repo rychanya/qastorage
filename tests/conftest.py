3d3da176921940b33e8c376f67c89f78762ffaea
rychanya <rychanya@gmail.ru>
2022-01-06 08:05:43 +0300

    add test containers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,19 @@
 import pytest
 from pymongo import MongoClient
+from testcontainers.mongodb import MongoDbContainer
 
-from qastorage.mongo_srore import MongoStore
+from storage.mongo_store import MongoStore
 
 TEST_DB_NAME = "test"
 
 
 @pytest.fixture
 def mongo_client():
-    client = MongoClient(host="mongo", uuidRepresentation="standard")
-    yield client
-    client.close()
+    with MongoDbContainer() as mongo:
+        url = mongo.get_connection_url()
+        client = MongoClient(host=url, uuidRepresentation="standard")
+        yield client
+        client.close()
 
 
 @pytest.fixture

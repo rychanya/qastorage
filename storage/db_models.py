f81eb9f12de4d5e18203b9c25f8767df10e520a6
rychanya <rychanya@gmail.ru>
2022-01-11 20:37:18 +0300

    change field ordering

--- storage/db_models.py
+++ storage/db_models.py
@@ -7,16 +7,16 @@ from storage.dto import ConStr, QATypeEnum
 
 
 class QABase(BaseModel):
-    question: ConStr
-    type: QATypeEnum
     id: UUID = Field(default_factory=uuid4)
+    type: QATypeEnum
+    question: ConStr
 
 
 class QAGroup(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    base_id: UUID
     all_answers: list[ConStr]
     all_extra: list[ConStr] = []
-    base_id: UUID
-    id: UUID = Field(default_factory=uuid4)
 
 
 class QAAnswer(BaseModel):

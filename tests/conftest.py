d7ac5505712805a57f35a429c3ece2c249d4c83a
rychanya <rychanya@gmail.ru>
2022-01-19 13:14:24 +0300

    replace mongo test container with subproc

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,44 @@
+import subprocess
+from tempfile import TemporaryDirectory
+
 import pytest
-from testcontainers.mongodb import MongoDbContainer
 
 from storage import StoreType
 from storage import get_store as get_storage
 from storage.mongo_store import MongoStore
 
 
-@pytest.fixture(scope="session")
-def mongo_connection_url():
-    _mongo = MongoDbContainer().start()
-    yield _mongo.get_connection_url()
-    _mongo.stop()
+@pytest.fixture(scope="session", autouse=True)
+def mongo_client():
+    with TemporaryDirectory() as tempdir:
+        with subprocess.Popen(
+            [
+                "mongod",
+                "--replSet",
+                "rs0",
+                "--dbpath",
+                tempdir,
+                "--bind_ip",
+                "localhost",
+            ],
+            stdout=subprocess.DEVNULL,
+        ) as mongo:
+            subprocess.run(
+                ["mongosh", "--eval", "rs.initiate()"], stdout=subprocess.DEVNULL
+            )
 
+            yield
 
-# @pytest.fixture
-# def mongo_settings(monkeypatch, mongo_connection_url):
-#     monkeypatch.setenv("qa_storage_url", mongo_connection_url)
-#     monkeypatch.setenv("qa_storage_db_name", "test")
+            mongo.terminate()
+            mongo.wait()
 
 
 @pytest.fixture
-def get_store(monkeypatch, mongo_connection_url):
+def get_store(monkeypatch):
     def _get_store(store_name: str):
         if store_name == StoreType.Mongo:
             db_name = "test"
             monkeypatch.setenv("qa_storage_type", StoreType.Mongo)
-            monkeypatch.setenv("qa_storage_url", mongo_connection_url)
             monkeypatch.setenv("qa_storage_db_name", db_name)
             _store = get_storage()
             if not isinstance(_store, MongoStore):
@@ -39,12 +52,11 @@ def get_store(monkeypatch, mongo_connection_url):
 
 
 @pytest.fixture
-def set_store_settings(monkeypatch, mongo_connection_url):
+def set_store_settings(monkeypatch):
     def _get_store(store_name: str):
         if store_name == StoreType.Mongo:
             db_name = "test"
             monkeypatch.setenv("qa_storage_type", StoreType.Mongo)
-            monkeypatch.setenv("qa_storage_url", mongo_connection_url)
             monkeypatch.setenv("qa_storage_db_name", db_name)
         else:
             monkeypatch.setenv("qa_storage_type", store_name)
